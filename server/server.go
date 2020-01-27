2d352aa22a6b9de0cdee756b72b6e3542d76b573
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-28 00:38:57 +0700

    added config database into server

--- server/server.go
+++ server/server.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/handler"
 	go_gqlgen "github.com/confus1on/meetmeup"
+	"github.com/confus1on/meetmeup/ent"
+	"github.com/confus1on/meetmeup/postgres"
 )
 
 const defaultPort = "8080"
 
+var client *ent.Client
+
 func main() {
+	client, err := postgres.Open()
+	if err != nil {
+		log.Fatalf("Error connected to PostgreSQL %+v ", err)
+	}
+
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	config := go_gqlgen.Config{Resolvers: &go_gqlgen.Resolver{
+		MeetupsRepo: postgres.MeetupsRepository{Client: client},
+		UsersRepo:   postgres.UsersRepository{Client: client},
+	}}
+
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(go_gqlgen.NewExecutableSchema(go_gqlgen.Config{Resolvers: &go_gqlgen.Resolver{}})))
+	http.Handle("/query", handler.GraphQL(go_gqlgen.NewExecutableSchema(config)))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
