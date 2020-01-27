0925e0270bb53b756ed4f5777534e4ebf0a7e2f4
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-27 20:11:07 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/confus1on/meetmeup
+
+go 1.12
+
+require (
+	github.com/99designs/gqlgen v0.10.2
+	github.com/vektah/gqlparser v1.2.0
+)
---
+++ resolver.go
@@ -0,0 +1,17 @@
+package go_gqlgen
+
+import (
+	"context"
+) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+type Resolver struct{}
+
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Meetups(ctx context.Context) ([]*Meetup, error) {
+	panic("not implemented")
+}
